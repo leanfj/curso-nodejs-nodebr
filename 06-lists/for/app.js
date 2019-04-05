7f0a1421f94c99982623cf754ddf3c1b3e8c38af
leanfj <leanfj@hotmail.com>
2019-04-05 06:39:22 -0300

    Itrando por lista com lacos for

--- 06-lists/for/app.js
+++ 06-lists/for/app.js
@@ -2,13 +2,35 @@ const service = require("./service");
 
 (async function main() {
   try {
-    const resultado = await service.obterPessoas("sky");
+    const resultado = await service.obterPessoas("a");
+
     const nomes = [];
+
+    console.time("for");
 
     for (i = 0; i <= resultado.results.length - 1; i++) {
       const pessoa = resultado.results[i];
       nomes.push(pessoa.name);
     }
+
+    console.timeEnd("for");
+    console.time("forin");
+
+    for (let i in resultado.results) {
+      const pessoa = resultado.results[i];
+
+      nomes.push(pessoa.name);
+    }
+
+    console.timeEnd("forin");
+
+    console.time("forof");
+
+    for (pessoa of resultado.results) {
+      nomes.push(pessoa.name);
+    }
+
+    console.timeEnd("forof");
 
     console.log("nomes", nomes);
   } catch (error) {
