e3275b8dfc57ebac6decf16537221f0eadd1e42b
leanfj <leanfj@hotmail.com>
2019-05-06 06:27:07 -0300

    Criado testes e strategie para mongodb

--- 11-multi-db-mongo/src/db/strategies/mongodb.js
+++ 11-multi-db-mongo/src/db/strategies/mongodb.js
@@ -1,15 +1,84 @@
 const ICRUD = require("./interfaces/interfaceCRUD");
+const mongoose = require("mongoose");
+
+const statusConnection = {
+  0: "Disconectado",
+  1: "Conectado",
+  2: "Conectando",
+  3: "Desconectado"
+};
 
 class MongoDB extends ICRUD {
   constructor() {
     super();
+    this._facs = null;
+    this._DRIVER = null;
   }
 
-  isConnected() {}
+  async isConnected() {
+    const state = statusConnection[this._DRIVER.readyState];
+
+    if (state === "Conectado") {
+      return state;
+    }
+
+    if (state !== "Conectando") {
+      return state;
+    }
+
+    await new Promise(resolve => {
+      setTimeout(resolve, 1000);
+    });
+
+    return statusConnection[this._DRIVER.readyState];
+  }
+
+  defineModel() {
+    const facSchema = new mongoose.Schema({
+      fac: {
+        type: Number,
+        required: true
+      },
+      category: {
+        type: Number,
+        required: true
+      },
+      insertedAt: {
+        type: Date,
+        default: new Date()
+      }
+    });
+
+    this._facs = mongoose.model("facs", facSchema);
+  }
+
+  connect() {
+    mongoose.connect(
+      "mongodb://leanfj:senhasecreta@localhost:27017/facs",
+      { useNewUrlParser: true },
+      error => {
+        if (!error) {
+          return;
+        }
+        console.log("Falha ao conectar no banco", error);
+      }
+    );
+
+    const connection = mongoose.connection;
+    connection.once("open", () => console.log("Conexão realizado com sucesso"));
+
+    this._DRIVER = connection;
+  }
 
   create(item) {
     console.log("O item foi salvo no MongoDB");
   }
+
+  read(item = {}) {}
+
+  update(id, item) {}
+
+  remove(id) {}
 }
 
 module.exports = MongoDB;
---
+++ 11-multi-db-mongo/src/tests/mongoStrategy.js
@@ -0,0 +1,29 @@
+const assert = require("assert");
+const MongoDB = require("../db/strategies/mongodb");
+const Context = require("../db/strategies/base/contextStrategy");
+
+const mongoStrategy = new Context(new MongoDB());
+
+const MOCK_FAC = {
+  fac: 555666,
+  category: 40
+};
+
+const MOCK_FAC_UPDATE = {
+  fac: 444333,
+  category: 40
+};
+
+describe("Mongo Strategy", function() {
+  this.timeout(Infinity);
+
+  this.beforeAll(async function() {
+    await mongoStrategy.connect();
+  });
+
+  it("Should connect in Mongo Database", async function() {
+    const result = await mongoStrategy.isConnected();
+    const expected = "Conectado";
+    assert.equal(result, expected);
+  });
+});
